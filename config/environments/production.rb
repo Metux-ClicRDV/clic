19cd9dc22577eedfeaffecb1a4350feb301eb887
Julien <julien.collet01@gmail.com>
2014-06-20 13:41:37 +0200

    disable minification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,10 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = false
   # config.assets.css_compressor = :sass
+
+  config.assets.enabled = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
